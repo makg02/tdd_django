70d70c8544f50f161ab369311f00b484b2d985a2
Mack <gayonmarkanthony@gmail.com>
2018-10-21 14:00:51 -0800

    changes ft

--- functional_tests.py
+++ functional_tests.py
@@ -19,6 +19,11 @@ class NewVisitorTest(unittest.TestCase): #1
     def tearDown(self): #3
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retreive_it_later(self): #4
         #Edith has heard about a cool new online
         #to check out its homepage
@@ -30,10 +35,24 @@ class NewVisitorTest(unittest.TestCase): #1
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+
+
         #She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+
 
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby)
