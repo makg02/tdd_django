cf2c5780404ceb80fcc8ae5f2daee88285acccbe
Mack <gayonmarkanthony@gmail.com>
2018-10-21 17:04:58 -0800

    URLs for forms x2, moved in views + tests, new URL

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page #1
+from lists.models import Item
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -22,6 +23,22 @@ class HomePageTest(TestCase):
         #self.assertIn(b'<title>To-Do lists</title>', response.content)
         #self.assertTrue(response.content.strip().endswith(b'</html>'))
 
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+
+    #def test_home_page_displays_all_list_items(self):
+    #    Item.objects.create(text ='itemey 1')
+    #    Item.objects.create(text ='itemey 2')
+    #    request = HttpRequest()
+    #    response = home_page(request)
+
+    #    self.assertIn('itemey 1', response.content.decode())
+    #    self.assertIn('itemey 2', response.content.decode())
+
+
 class ItemModelTest(TestCase):
 
     def text_saving_and_retrieving_items(self):
@@ -39,3 +56,38 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
+
+        self.client.post(
+            '/lists/new',
+            data={'item_text' : 'A new list item'}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text' : 'A new list item'}
+        )
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
